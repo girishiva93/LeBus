4d1ab85823ed95c10d1b6288f52bbf112ee7dab8
{shiva} <girishiva93@gmail.com>
2020-09-26 20:26:31 +0545

    update

--- js/index.js
+++ js/index.js
@@ -16,3 +16,32 @@ tl.staggerFrom([line1, line2, line3], 1, {
     opacity: 0,
     y:44,
 })
+
+function displayList(){
+    var responsive = document.getElementById('nav-responsive-items');
+    responsive.classList.toggle("nav-responsive-active")
+  }
+  // owl carousel js start
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin:30,
+      nav: true,
+      autoplay: true,
+      animateIn: "fadeIn",
+      animateOut: "fadeOut",
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  });
+  // owl carousel js end
