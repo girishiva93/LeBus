161e89ec196a2a3ca78546be941da6c4c3801bd2
{shiva} <girishiva93@gmail.com>
2020-09-28 20:42:21 +0545

    update

--- js/index.js
+++ js/index.js
@@ -33,10 +33,10 @@ $(document).ready(function () {
       responsiveClass: true,
       responsive: {
         0: {
-          items: 1,
+          items: 3,
         },
         600: {
-          items: 2,
+          items: 4,
         },
         1000: {
           items: 10,
